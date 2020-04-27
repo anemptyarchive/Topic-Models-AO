7ae09214cfa10b7775efeb73ca80ae256117fcb3
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-04-27 16:17:07 +0900

    update

--- Code/Code_ch3_3.R
+++ Code/Code_ch3_3.R
@@ -1,8 +1,17 @@
 
-# Chapter3.3 混合ユニグラムモデル：最尤推定 ----------------------------------------------------------------------
+# Chapter3.3 混合ユニグラムモデル：EMアルゴリズム(最尤推定) ----------------------------------------------------------------------
 
 # 利用パッケージ
 library(tidyverse)
+
+
+## 簡易文書データ
+# 文書数
+D <- 10
+# 語彙数
+V <- 20
+# 各文書における各語彙数
+N_dv <- matrix(sample(1:10, D * V, replace = TRUE), D, V)
 
 
 # パラメータの初期設定 --------------------------------------------------------------
@@ -11,55 +20,38 @@ library(tidyverse)
 K <- 5
 
 # 負担率(q_dk)の受け皿
-q_dk <- matrix(0, nrow = D, ncol = K, 
-               dimnames = list(paste0("d=", 1:D),  # 行名
-                               paste0("k=", 1:K))) # 列名
 q_dk <- matrix(
   0, nrow = D, ncol = K, 
-  dimnames = list(
-    paste0("d=", 1:D), # 行名
-    paste0("k=", 1:K)  # 列名
-  )
+  dimnames = list(paste0("d=", 1:D), # 行名
+                  paste0("k=", 1:K)) # 列名
 )
 
-
-## トピック分布(theta)
-# 初期値をランダムに設定
-tmp_theta <- seq(0, 1, by = 0.01) %>% 
+## トピック分布(theta)の初期値
+# ランダムに値を設定
+tmp_theta_k <- seq(0, 1, by = 0.01) %>% 
   sample(size = K, replace = TRUE)
 
 # 正規化
-theta_k <- tmp_theta / sum(tmp_theta)
+theta_k <- tmp_theta_k / sum(tmp_theta_k)
 
 # 確認用の要素名
 names(theta_k) <- paste0("k=", 1:K)
 
-
-## 単語分布(phi)
-# 初期値をランダムに設定
-phi_kv <- seq(0, 1, by = 0.01) %>% 
-  sample(size = K * V, replace = TRUE) %>% 
-  matrix(nrow = K, ncol = V, 
-         dimnames = list(paste0("k=", 1:K),  # 行名
-                         paste0("v=", 1:V))) # 列名
-phi_kv <- seq(0, 1, by = 0.01) %>% 
+## 単語分布(phi)の初期値
+# ランダムに値を設定
+tmp_phi_kv <- seq(0, 1, by = 0.01) %>% 
   sample(size = K * V, replace = TRUE) %>% 
   matrix(
     nrow = K, ncol = V, 
-    dimnames = list(
-      paste0("k=", 1:K), # 行名
-      paste0("v=", 1:V)  # 列名
-    )
+    dimnames = list(paste0("k=", 1:K), # 確認用の行名
+                    paste0("v=", 1:V)) # 確認用の列名
   )
 
 # 正規化
-phi_kv <- phi_kv / apply(phi_kv, 1, sum)
-for(k in 1:K) {
-  phi_kv[k, ] <- phi_kv[k, ] / sum(phi_kv[k, ])
-}
+phi_kv <- tmp_phi_kv / apply(tmp_phi_kv, 1, sum)
 
 
-# 最尤推定 --------------------------------------------------------------------
+# EMアルゴリズム(最尤推定) --------------------------------------------------------------------
 
 # 試行回数を指定
 Iter <- 50
@@ -82,14 +74,14 @@ for(i in 1:Iter){
   # 次ステップのパラメータを初期化
   new_theta_k <- rep(0, K)
   names(new_theta_k) <- paste0("k=", 1:K)
+  new_phi_kv <- matrix(
+    0, nrow = K, ncol = V, 
+    dimnames = list(paste0("k=", 1:K), paste0("v=", 1:V))
+  )
   
-  new_phi_kv <- matrix(0, nrow = K, ncol = V, 
-                       dimnames = list(paste0("k=", 1:K), 
-                                       paste0("v=", 1:V)))
-  
-  for(d in 1:D){ ## (各文書：1,...,D)
+  for(d in 1:D){ ## (各文書)
     
-    for(k in 1:K){ ## (各トピック：1,...,K)
+    for(k in 1:K){ ## (各トピック)
       
       # 負担率の計算
       tmp_phi_k <- apply(t(phi_kv)^N_dv[d, ], 2, prod)
@@ -98,7 +90,7 @@ for(i in 1:Iter){
       # トピック分布(theta)を更新
       new_theta_k[k] <- new_theta_k[k] + q_dk[d, k]
       
-      for(v in 1:V){ ## (各語彙：1,...,V)
+      for(v in 1:V){ ## (各語彙)
         
         # 単語分布(phi)を更新
         new_phi_kv[k, v] <- new_phi_kv[k, v] + q_dk[d, k] * N_dv[d, v]
@@ -119,13 +111,14 @@ for(i in 1:Iter){
   trace_theta[, i + 1] <- theta_k
   trace_phi[, , i + 1] <- phi_kv
   
+  # 動作確認
   print(paste0(i, "th try..."))
 }
 
 
 # 推定結果の確認 -----------------------------------------------------------------
 
-## トピック分布の推定結果を確認
+## トピック分布
 # 作図用のデータフレームを作成
 theta_df <- data.frame(
   topic = as.factor(1:K), 
@@ -139,7 +132,7 @@ ggplot(theta_df, aes(x = topic, y = prob, fill = topic)) +
        subtitle = "EM Algorithm") # ラベル
 
 
-## 単語分布の推定結果を確認
+## 単語分布
 # 作図用のデータフレームを作成
 phi_df_wide <- cbind(
   as.data.frame(phi_kv), 
@@ -149,27 +142,27 @@ phi_df_wide <- cbind(
 # データフレームをlong型に変換
 phi_df_long <- pivot_longer(
   phi_df_wide, 
-  cols = -topic, 
-  names_to = "word", 
-  names_prefix = "v=", 
-  names_ptypes = list(word = factor()), 
-  values_to = "prob"
+  cols = -topic, # 変換せずにそのまま残す現列名
+  names_to = "word", # 現列名を格納する新しい列の名前
+  names_prefix = "v=", # 現列名から取り除く文字列
+  names_ptypes = list(word = factor()), # 現列名を要素とする際の型
+  values_to = "prob" # 現要素を格納する新しい列の名前
 )
 
 # 作図
 ggplot(phi_df_long, aes(x = word,  y = prob, fill = word, color = word)) + 
-  geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
-  facet_wrap(~ topic, labeller = label_both) +       # グラフの分割
-  theme(legend.position = "none") +                  # 凡例
-  scale_x_discrete(breaks = seq(1, V, by = 10)) + # x軸目盛
+  geom_bar(stat = "identity", position = "dodge") + # 棒グラフ
+  facet_wrap(~ topic, labeller = label_both) + # グラフの分割
+  scale_x_discrete(breaks = seq(0, V, by = 10)) + # x軸目盛
+  theme(legend.position = "none") + # 凡例
   labs(title = "Mixture of Unigram Models", 
        subtitle = "EM Algorithm") # ラベル
 
 
 
-# 推移の確認 -------------------------------------------------------------------
+# 更新値の推移の確認 -------------------------------------------------------------------
 
-## トピック分布の推定値の推移を確認
+## トピック分布
 # 作図用のデータフレームを作成
 trace_theta_df_wide <- cbind(
   as.data.frame(trace_theta), 
@@ -179,11 +172,11 @@ trace_theta_df_wide <- cbind(
 # データフレームをlong型に変換
 trace_theta_df_long <- pivot_longer(
   trace_theta_df_wide, 
-  cols = -topic, 
-  names_to = "Iteration", 
-  names_prefix = "i=", 
-  names_ptypes = list(Iteration = numeric()), 
-  values_to = "prob"
+  cols = -topic, # 変換せずにそのまま残す現列名
+  names_to = "Iteration", # 現列名を格納する新しい列の名前
+  names_prefix = "i=", # 現列名から取り除く文字列
+  names_ptypes = list(Iteration = numeric()), # 現列名を要素とする際の型
+  values_to = "prob" # 現要素を格納する新しい列の名前
 )
 
 # 作図
@@ -193,7 +186,7 @@ ggplot(trace_theta_df_long, aes(x = Iteration, y = prob, color = topic)) +
        subtitle = "EM Algorithm") # ラベル
 
 
-## 単語分布の推定値の推移を確認
+## 単語分布
 # 確認するトピック番号を指定
 TopicNum <- 1
 
@@ -206,11 +199,11 @@ trace_phi_df_wide <- cbind(
 # データフレームをlong型に変換
 trace_phi_df_long <- pivot_longer(
   trace_phi_df_wide, 
-  cols = -word, 
-  names_to = "Iteration", 
-  names_prefix = "i=", 
-  names_ptypes = list(Iteration = numeric()), 
-  values_to = "prob"
+  cols = -word, # 変換せずにそのまま残す現列名
+  names_to = "Iteration", # 現列名を格納する新しい列の名前
+  names_prefix = "i=", # 現列名から取り除く文字列
+  names_ptypes = list(Iteration = numeric()), # 現列名を要素とする際の型
+  values_to = "prob" # 現要素を格納する新しい列の名前
 )
 
 # 作図
@@ -230,7 +223,7 @@ library(gganimate)
 ## トピック分布
 # 作図
 graph_theta <- ggplot(trace_theta_df_long, aes(x = topic, y = prob, fill = topic)) + 
-  geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
+  geom_bar(stat = "identity", position = "dodge") + # 棒グラフ
   transition_manual(Iteration) + 
   labs(title = "Mixture of Unigram Models:EM Algorithm", 
        subtitle = "i={current_frame}") # ラベル
@@ -258,72 +251,23 @@ for(i in 1:(Iter + 1)) {
 trace_phi_LongDF <- pivot_longer(
   trace_phi_WideDF, 
   cols = -c(topic, Iteration), # 変換せずにそのまま残す現列名
-  names_to = "word",   # 現列名を格納する新しい列の名前
-  names_prefix = "v=",  # 現列名から取り除く文字列
-  names_ptypes = list(word = factor()),  # 現列名を要素とする際の型
-  values_to = "prob"   # 現要素を格納する新しい列の名前
+  names_to = "word", # 現列名を格納する新しい列の名前
+  names_prefix = "v=", # 現列名から取り除く文字列
+  names_ptypes = list(word = factor()), # 現列名を要素とする際の型
+  values_to = "prob" # 現要素を格納する新しい列の名前
 )
 
 # 作図
 graph_phi <- ggplot(trace_phi_LongDF, aes(x = word, y = prob, fill = word, color = word)) + 
-  geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
-  facet_wrap( ~ topic, labeller = label_both) +      # グラフの分割
-  scale_x_discrete(breaks = seq(1, V, by = 10)) +    # x軸目盛
-  theme(legend.position = "none") +                  # 凡例
-  transition_manual(Iteration) + 
+  geom_bar(stat = "identity", position = "dodge") + # 棒グラフ
+  facet_wrap( ~ topic, labeller = label_both) + # グラフの分割
+  scale_x_discrete(breaks = seq(0, V, by = 10)) + # x軸目盛
+  theme(legend.position = "none") + # 凡例
+  transition_manual(Iteration) + # フレーム
   labs(title = "Mixture of Unigram Models:EM Algorithm", 
        subtitle = "i={current_frame}") # ラベル
 
-# 描画
+# gif画像を作成
 animate(graph_phi, nframes = Iter + 1, fps = 5)
-
-
-# 各トピックの出現確率の上位語 ---------------------------------------------------------------------
-
-
-# 文書インデックス(d)
-d_index <- list.files(file_path) %>% 
-  str_remove_all(".txt")
-
-
-## 語彙インデックス(v)
-# 指定した出現回数以上の単語の行番号
-num <- mecab_df %>% 
-  select(-c(TERM, POS1, POS2)) %>% 
-  apply(1, sum) >= 5 # 抽出する総出現回数を指定する
-
-v_index <- mecab_df[num, ] %>% 
-  filter(grepl("名詞|形容詞", POS1)) %>%  # 抽出する品詞を指定する|^動詞
-  filter(grepl("一般|^自立", POS2)) %>% 
-  filter(!grepl(stop_words, TERM)) %>% 
-  .[, 1]  # 単語の列を抽出する
-
-# データフレームを作成
-phi_df_wide2 <- cbind(as.data.frame(t(phi_kv)), 
-                      v_index) %>% 
-  as.data.frame()
-colnames(phi_df_wide2) <- c(paste0("topic", 1:K), "word") # key用の行名を付与
-
-# データフレームの整形
-phi_df_long2 <- data.frame()
-for(i in 1:K) {
-  tmp_df <- phi_df_wide2 %>% 
-    select(paste0("topic", i), word) %>% 
-    arrange(-.[, 1]) %>%  # 降順に並べ替え
-    head(20) %>%          # 任意で指定した上位n語を抽出
-    gather(key = "topic", value = "prob", -word) # long型に変換
-  
-  phi_df_long2 <- rbind(phi_df_long2, tmp_df)
-}
-
-
-# 描画
-ggplot(data = phi_df_long2, 
-       mapping = aes(x = reorder(x = word, X = prob), y = prob, fill = word)) +  # データ
-  geom_bar(stat = "identity", position = "dodge") + # 棒グラフ
-  coord_flip() +                                    # グラフの向き
-  facet_wrap( ~ topic, scales = "free") +           # グラフの分割
-  theme(legend.position = "none") +                 # 凡例
-  labs(title = "Mixture of Unigram Models:VBE")     # タイトル
 
 
--- Code/Code_ch3_4.R
+++ Code/Code_ch3_4.R
@@ -1,95 +1,77 @@
 
-# 混合ユニグラムモデル：変分ベイズ推定 ----------------------------------------------------------------------
-
+# ch3.4 混合ユニグラムモデル：変分ベイズ推定 ----------------------------------------------------------------------
 
 # 利用パッケージ
-library(RMeCab)
 library(tidyverse)
 
 
-# テキスト処理 ------------------------------------------------------------------
-
-
-# テキストファイルの保存先を指定する
-file_path <- "text_data/lyrics/juicejuice"
-
-
-# 抽出しない単語を指定
-stop_words <- "[a-z]"
-
-
-# 形態素解析
-mecab_df <- docDF(file_path, type = 1)
-
-
-# 文書dの語彙vの出現回数(N_dv)の集合
-N_dv <- mecab_df %>% 
-        filter(grepl("名詞|形容詞", POS1)) %>% # 抽出する品詞(大分類)を指定する|^動詞
-        filter(grepl("一般|^自立", POS2)) %>%  # 抽出する品詞(細分類)を指定する
-        filter(!grepl(stop_words, TERM)) %>%   # ストップワードを除く
-        select(-c(TERM, POS1, POS2)) %>%       # 数値列のみを残す
-        filter(apply(., 1, sum) >= 5) %>%      # 抽出する総出現回数を指定する
-        t()                                          # 転置
-
-# 確認用の行列名
-dimnames(N_dv) <- list(paste0("d=", 1:nrow(N_dv)), # 行名
-                       paste0("v=", 1:ncol(N_dv))) # 列名
-
-# 文書dの単語数(N_d)のベクトル
-N_d <- apply(N_dv, 1, sum)
-
-# 文書数(D)
-D <- nrow(N_dv)
-
-# 総語彙数(V)
-V <- ncol(N_dv)
+## 簡易文書データ
+# 文書数
+D <- 10
+# 語彙数
+V <- 20
+# 各文書における各語彙数
+N_dv <- matrix(sample(1:10, D * V, replace = TRUE), D, V)
 
 
 # パラメータの初期設定 --------------------------------------------------------------
 
+# トピック数(K)を指定
+K <- 4
 
-# トピック数(K)
-K <- 4 # 任意の値を指定する
+# 負担率(q_dk)の受け皿
+q_dk <- matrix(
+  0, nrow = D, ncol = K, 
+  dimnames = list(paste0("d=", 1:D), # 確認用の行名
+                  paste0("k=", 1:K)) # 確認用の列名
+)
 
+# 事前分布のパラメータ(α, β)を指定
+alpha <- 2
+beta  <- 2
 
-# 負担率(q_dk)の集合
-q_dk <- matrix(0, nrow = D, ncol = K, 
-               dimnames = list(paste0("d=", 1:D),  # 行名
-                               paste0("k=", 1:K))) # 列名
+# 変分事後分布のパラメータ(α_k)
+alpha_k <- seq(1, 3, 0.1) %>% # 範囲を指定
+  sample(size = K, replace = TRUE)
+# 確認用の要素名
+names(alpha_k) <- paste0("k=", 1:K)
 
-
-# 事前分布のパラメータ(α, β)
-alpha <- 2 # 任意の値を指定する
-beta  <- 2 # 任意の値を指定する
-
-
-# 変分事後分布のパラメータ(α_k)のベクトル
-alpha_k <- seq(1, 3, 0.1) %>%               # 任意の範囲を指定する
-           sample(size = K, replace = TRUE) # 値をランダムに割り振る
-names(alpha_k) <- paste0("k=", 1:K)         # 確認用の要素名
-
-# 変分事後分布のパラメータ(β_kv)の集合
-beta_kv <- seq(1, 3, 0.1) %>%                        # 任意の範囲を指定する
-           sample(size = K * V, replace = TRUE) %>%  # 値をランダムに割り振る
-           matrix(nrow = K, ncol = V, 
-                  dimnames = list(paste0("k=", 1:K),  # 行名
-                                  paste0("v=", 1:V))) # 列名
+# 変分事後分布のパラメータ(β_kv)
+beta_kv <- seq(1, 3, 0.1) %>% # 範囲を指定
+  sample(size = K * V, replace = TRUE) %>% 
+  matrix(
+    nrow = K, ncol = V, 
+    dimnames = list(paste0("k=", 1:K), # 行名
+                    paste0("v=", 1:V)) # 列名
+  )
 
 
 # 変分ベイズ推定 -----------------------------------------------------------------
 
+# 試行回数を指定
+Iter <- 50
 
 # 推移の確認用
-trace_alpha <- as.matrix(alpha_k)
-trace_beta  <- beta_kv[1, ]
+trace_alpha <- matrix(
+  0, nrow = K, ncol = Iter + 1, 
+  dimnames = list(paste0("k=", 1:K), paste0("i=", 0:Iter))
+)
+trace_beta  <- array(
+  0, dim = c(K, V, Iter + 1), 
+  dimnames = list(paste0("k=", 1:K), paste0("v=", 1:V), paste0("i=", 0:Iter))
+)
+# 初期値を代入
+trace_alpha[, 1]  <- alpha_k
+trace_beta[, , 1] <- beta_kv
 
-for(i in 1:50) { # 任意の回数を指定する
+for(i in 1:Iter) {
   
   # 次ステップのハイパーパラメータ(alpha, beta)を初期化
   new_alpha_k <- rep(alpha, K)
-  new_beta_kv <- matrix(beta, nrow = K, ncol = V, 
-                        dimnames = list(paste0("k=", 1:K), 
-                                        paste0("v=", 1:V)))
+  new_beta_kv <- matrix(
+    beta, nrow = K, ncol = V, 
+    dimnames = list(paste0("k=", 1:K), paste0("v=", 1:V))
+  )
   
   for(d in 1:D){ ## (各文書：1,...,D)
     
@@ -118,88 +100,110 @@ for(i in 1:50) { # 任意の回数を指定する
         # ハイパーパラメータ(beta_kv)を更新
         new_beta_kv[k, v] <- new_beta_kv[k, v] + q_dk[d, k] * N_dv[d, v]
         
-      } ## (/各語彙：1,...,V)
-    } ## (/各トピック：1,...,K)
-  } ## (/各文書：1,...,D)
+      } ## (/各語彙)
+    } ## (/各トピック)
+  } ## (/各文書)
   
   # ハイパーパラメータの(alpha, beta)を更新
   alpha_k <- new_alpha_k
   beta_kv <- new_beta_kv
   
-  # 推移の確認用
-  trace_alpha <- cbind(trace_alpha, alpha_k)
-  trace_beta  <- cbind(trace_beta, beta_kv[1, ])
+  # 初期値を代入
+  trace_alpha[, i + 1]  <- alpha_k
+  trace_beta[, , i + 1] <- beta_kv
 }
 
 
 # ハイパーパラメータの推定結果の確認 -----------------------------------------------------------------
 
-## ハイパーパラメータ(α)の推定結果を確認
-# データフレームを作成
-alpha_df <- data.frame(topic = as.factor(1:K), 
-                       alpha = alpha_k)
+## トピック分布パラメータ(α)
+# 作図用のデータフレームを作成
+alpha_df <- data.frame(
+  alpha = alpha_k, 
+  topic = as.factor(1:K)
+)
 
 # 作図
-ggplot(data = alpha_df, mapping = aes(x = topic, y = alpha, fill = topic)) + # データ
-  geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
-  labs(title = "Mixture of Unigram Models:VBE")      # タイトル
+ggplot(alpha_df, aes(x = topic, y = alpha, fill = topic)) + 
+  geom_bar(stat = "identity", position = "dodge") + # 棒グラフ
+  labs(title = "Mixture of Unigram Models", 
+       subtitle = "Variational Bayesian Estimation") # ラベル
 
 
-## ハイパーパラメータ(β)の推定結果を確認
-# データフレームを作成
-beta_df_wide <- cbind(as.data.frame(beta_kv), as.factor(1:K))
-colnames(beta_df_wide) <- c(1:V, "topic")
-beta_df_long <- gather(beta_df_wide, key = "term", value = "beta", -topic)
-beta_df_long$term <- as.factor(as.numeric(beta_df_long$term))
-
-# 作図
-ggplot(data = beta_df_long, mapping = aes(x = term, y = beta, fill = term)) + # データ
-  geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
-  facet_wrap(~topic, labeller = label_both) +        # グラフの分割
-  theme(legend.position = "none") +                  # 凡例
-  labs(title = "Mixture of Unigram Models:VBE")      # タイトル
-
-
-# パラメータの推定結果の確認 -----------------------------------------------------------
-
-## トピック分布の推定結果(平均値)を確認
-# パラメータの平均値を算出
-theta_k  <- alpha_k / sum(alpha_k) # 平均値を計算
-
-# データフレームを作成
-theta_df <- data.frame(topic = as.factor(1:K), 
-                       prob = theta_k)
-
-# 描画
-ggplot(data = theta_df, mapping = aes(x = topic, y = prob, fill = topic)) + # データ
-  geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
-  labs(title = "Mixture of Unigram Models:VBE")      # タイトル
-
-
-## 単語分布の推定結果(平均値)を確認
-# パラメータの平均値を算出
-phi_kv <- matrix(nrow = K, ncol = V)
-for(k in 1:K) {
-  phi_kv[k, ] <- beta_kv[k, ] / sum(beta_kv[k, ])
-}
-
-# データフレームを作成
-phi_df_wide <- cbind(as.data.frame(phi_kv), 
-                     as.factor(1:K))
+## 単語分布パラメータ(β)
+# 作図用のデータフレームを作成
+beta_df_wide <- cbind(
+  as.data.frame(beta_kv), 
+  topic = as.factor(1:K)
+)
 
 # データフレームをlong型に変換
-colnames(phi_df_wide) <- c(1:V, "topic") # key用の行名を付与
-phi_df_long <- gather(phi_df_wide, key = "word", value = "prob", -topic) # 変換
-phi_df_long$term <- phi_df_long$word %>% 
-                    as.numeric() %>% 
-                    as.factor()
+beta_df_long <- pivot_longer(
+  beta_df_wide, 
+  cols = -topic, # 変換せずにそのまま残す現列名
+  names_to = "word", # 現列名を格納する新しい列の名前
+  names_prefix = "v=", # 現列名から取り除く文字列
+  names_ptypes = list(word = factor()), # 現列名を要素とする際の型
+  values_to = "value" # 現要素を格納する新しい列の名前
+)
 
-# 描画
-ggplot(data = phi_df_long, mapping = aes(x = word,  y = prob, fill = word)) + # データ
-  geom_bar(stat = "identity", position = "dodge") +  # 棒グラフ
-  facet_wrap(~ topic, labeller = label_both) +       # グラフの分割
-  theme(legend.position = "none") +                  # 凡例
-  labs(title = "Mixture of Unigram Models:VBE")      # タイトル
+# 作図
+ggplot(beta_df_long, aes(x = word, y = value, fill = word, color = word)) + 
+  geom_bar(stat = "identity", position = "dodge") + # 棒グラフ
+  facet_wrap(~ topic, labeller = label_both) + # グラフの分割
+  scale_x_discrete(breaks = seq(0, V, by = 10)) + # x軸目盛
+  theme(legend.position = "none") + # 凡例
+  labs(title = "Mixture of Unigram Models", 
+       subtitle = "Variational Bayesian Estimation") # ラベル
+
+
+# 推定結果の確認 -----------------------------------------------------------
+
+## トピック分布(平均値)
+# ディリクレ分布の平均値を計算:式(1.15)
+theta_k  <- alpha_k / sum(alpha_k)
+
+# 作図用のデータフレームを作成
+theta_df <- data.frame(
+  prob = theta_k, 
+  topic = as.factor(1:K)
+)
+
+# 作図
+ggplot(theta_df, aes(x = topic, y = prob, fill = topic)) + 
+  geom_bar(stat = "identity", position = "dodge") + # 棒グラフ
+  labs(title = "Mixture of Unigram Models", 
+       subtitle = "Variational Bayesian Estimation") # ラベル
+
+
+## 単語分布(平均値)
+# ディリクレ分布の平均値を計算:式(1.15)
+phi_kv <- beta_kv / apply(beta_kv, 1, sum)
+
+# 作図用のデータフレームを作成
+phi_df_wide <- cbind(
+  as.data.frame(phi_kv), 
+  topic = as.factor(1:K)
+)
+
+# データフレームをlong型に変換
+phi_df_long <- pivot_longer(
+  phi_df_wide, 
+  cols = -topic, # 変換せずにそのまま残す現列名
+  names_to = "word", # 現列名を格納する新しい列の名前
+  names_prefix = "v=", # 現列名から取り除く文字列
+  names_ptypes = list(word = factor()), # 現列名を要素とする際の型
+  values_to = "prob" # 現要素を格納する新しい列の名前
+)
+
+# 作図
+ggplot(phi_df_long, aes(x = word,  y = prob, fill = word, color = word)) + 
+  geom_bar(stat = "identity", position = "dodge") + # 棒グラフ
+  facet_wrap(~ topic, labeller = label_both) + # グラフの分割
+  scale_x_discrete(breaks = seq(0, V, by = 10)) + # x軸目盛
+  theme(legend.position = "none") + # 凡例
+  labs(title = "Mixture of Unigram Models", 
+       subtitle = "Variational Bayesian Estimation") # ラベル
 
 
 # 推移の確認 -------------------------------------------------------------------
