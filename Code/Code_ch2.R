611f25bbc15500934e7a9e10aa2218bce8f8e8f7
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-07-02 00:20:18 +0900

    update chapter2

--- Code/Code_ch2.R
+++ Code/Code_ch2.R
@@ -31,10 +31,10 @@ doc_df2 <- tibble(v = 1:V) %>% # 1からVまでの受け皿(行)を作成
 
 # 最尤推定
 ml_df <- doc_df2 %>% 
-  mutate(phi_v = N_v / sum(N_v)) # 式(4.2)
+  mutate(prob = N_v / sum(N_v)) # 式(4.2)
 
 # 作図
-ggplot(ml_df, aes(x = v, y = phi_v)) + # データ
+ggplot(ml_df, aes(x = v, y = prob)) + # データ
   geom_bar(stat = "identity", fill = "#00A968") + # 棒グラフ
   scale_x_continuous(breaks = 1:V, labels = 1:V) + # 軸目盛
   labs(title = "Unigram Model", 
@@ -84,8 +84,8 @@ map_df <- doc_df2 %>%
 ggplot(map_df, aes(x = v, y = prob)) + # データ
   geom_bar(stat = "identity", fill = "#00A968") + # 棒グラフ
   scale_x_continuous(breaks = 1:V, labels = 1:V) + # 軸目盛
-  labs(title = "Unigram Model", 
-       subtitle = "Maximum A Posteriori Estimation") # ラベル
+  labs(title = "Unigram Model:Maximum A Posteriori Estimation", 
+       subtitle = paste0("N=", N_d)) # ラベル
 
 
 # 最尤推定とMAP推定の比較 -----------------------------------------------------------
@@ -133,8 +133,8 @@ res_df_long <- pivot_longer(
 # 作図
 ggplot(res_df_long, aes(x = v, y = prob, fill = method)) + # データの設定
   geom_bar(stat = "identity") + # 棒グラフ
-  facet_wrap(method ~ N, nrow = 2) + # グラフの分割
-  #facet_wrap(N ~ method, ncol = 2) + # グラフの分割
+  facet_wrap(method ~ N, nrow = 2, labeller = label_both) + # グラフの分割
+  #facet_wrap(N ~ method, ncol = 2, labeller = label_both) + # グラフの分割
   scale_x_continuous(breaks = 1:V, labels = 1:V) + # 軸目盛
   scale_fill_manual(values = c("#00A968", "Orange")) + # 塗りつぶし色
   labs(title = "Unigram Model") # ラベル
@@ -197,15 +197,13 @@ ggplot(bayes_df, aes(x = x, y = y, color = density)) + # データ
   scale_x_continuous(breaks = c(0, 1), labels = c("(1, 0, 0)", "(0, 1, 0)")) + # x軸目盛
   scale_y_continuous(breaks = c(0, 0.87), labels = c("(1, 0, 0)", "(0, 0, 1)")) + # y軸目盛
   coord_fixed(ratio = 1) + # 縦横の比率
-  labs(title = "Unigram Model", 
-       subtitle = "Bayesian Estimation", 
+  labs(title = "Unigram Model:Bayesian Estimation", 
+       subtitle = paste0("N=", N_d), 
        x = expression(paste(phi[1], ", ", phi[2], sep = "")), 
        y = expression(paste(phi[1], ", ", phi[3], sep = ""))) # ラベル
 
 
 # ch2.6 ベイズ予測分布 --------------------------------------------------------------
-
-# 通常 ----------------------------------------------------------------------
 
 # 利用パッケージ
 library(tidyverse)
@@ -239,68 +237,80 @@ doc_df2 <- tibble(v = 1:V) %>% # 1からVまでの受け皿(行)を作成
   mutate(N_v = replace_na(N_v, 0)) # NAを0に置換
 
 # ベイズ予測分布を計算
-predict_df <- tibble(
-  v = 1:V, # 語彙インデックス
-  prob = (doc_df2[["N_v"]] + beta) / sum(doc_df2[["N_v"]] + beta) # 確率
-)
+predict_df <- doc_df2 %>% 
+  mutate(prob = (N_v + beta) / sum(N_v + beta)) # 確率
 
 # 作図
 ggplot(predict_df, aes(x = v, y = prob)) + # データ
   geom_bar(stat = "identity", fill = "#00A968") + # 棒グラフ
   scale_x_continuous(breaks = 1:V, labels = 1:V) + # 軸目盛
-  labs(title = "Unigram Model", 
-       subtitle = "Bayesian Predictive Distribution") # ラベル
+  labs(title = "Unigram Model:Bayesian Predictive Distribution", 
+       subtitle = paste0("N=", N_d)) # ラベル
 
 
-# 複数 ----------------------------------------------------------------------
+# ch2.7 ハイパーパラメータ推定 --------------------------------------------------------------
 
-bpd_long <- gather(bpd_result, key = "tag", value = "prob", -v)
+# 不動点反復法 ------------------------------------------------------------------
 
-# 描画
-ggplot(data = bpd_long, mapping = aes(x = tag, y = prob, fill = tag)) + # データの指定
-  geom_bar(stat = "identity", position = "dodge") +    # 棒グラフ
-  ylim(c(0, 1)) +                                      # y軸の最小値・最大値
-  scale_fill_manual(values = c("Orange", "#00A968")) + # 色
-  facet_wrap( ~ v, labeller = label_both)              # グラフの分割
-
-
-
-# chapter2.7 --------------------------------------------------------------
-
-#### 不動点反復法####
+# ディガンマ関数 -----------------------------------------------------------------
 
 # 利用パッケージ
 library(tidyverse)
 
 
-# 対数をとったガンマ関数の曲線
-log_Gamma <- tibble(x_logGamma = seq(0.1, 10, 0.1),  # 作図用連続値：x
-                    y_logGamma = lgamma(x_logGamma))
+# 作図範囲を指定
+x_plot <- seq(0.01, 10, 0.01)
 
-# 曲線上の2点
-x_points <- c(2.5, 5)        # 曲線上の2点を指定する：x
-y_points <- lgamma(x_points) # 曲線上の2点を指定する：y
-points <- data.frame(x = x_points, y = y_points)
+
+# ディガンマ関数
+digamma_df <- tibble(
+  x = x_plot, 
+  y = digamma(x)
+)
+
+# 作図
+ggplot(digamma_df, aes(x = x, y = y)) + 
+  geom_line() + 
+  labs(title = "Digamma Function", 
+       y = expression(paste(psi, "(x)", sep = ""))) # ラベル
+
+
+# ディガンマ関数の性質 --------------------------------------------------------------
+
+# 対数をとったガンマ関数の曲線
+log_Gamma_df <- tibble(
+  x = x_plot, # 作図用のxの値
+  y = lgamma(x) # 対数をとったガンマ関数の値
+)
+
+# 曲線上の2点を指定
+point_df <- tibble(
+  x = c(2.5, 5), # 2点のx軸の値を指定
+  y = lgamma(x)
+)
 
 # 曲線上の2点を結ぶ直線
-a_line <- (lgamma(x_points[2]) - lgamma(x_points[1])) / (x_points[2] - x_points[1]) # 傾き：a
-b_line <- y_points[1] - a_line * x_points[1]                                        # 切片：b
-straight_line <- tibble(x_line = seq(0.1, 10, 0.1),        # 作図用連続値：x
-                        y_line = a_line * x_line + b_line)
+a_line <- (lgamma(point_df[["x"]][2]) - lgamma(point_df[["x"]][1])) / (point_df[["x"]][2] - point_df[["x"]][1]) # 傾き
+b_line <- point_df[["y"]][1] - a_line * point_df[["x"]][1] # 切片
+straight_line_df <- tibble(
+  x = x_plot, 
+  y = a_line * x + b_line
+)
 
-# 曲線上の点1を通る接線
-b_tangent <- y_points[1] - digamma(x_points[1]) * x_points[1] # 切片：b
-tangent <- tibble(x_tangent = seq(0.1, 10, 0.1),              # 作図用連続値：x
-                  y_tangent = digamma(x_points[1]) * x_tangent + b_tangent)
+# 接線
+b_tangent <- point_df[["y"]][1] - digamma(point_df[["x"]][1]) * point_df[["x"]][1] # 切片
+tangent_df <- tibble(
+  x = x_plot, 
+  y = digamma(point_df[["x"]][1]) * x + b_tangent
+)
 
-# 描画
+
+# 作図
 ggplot() + 
-  geom_line(data = log_Gamma, mapping = aes(x = x_logGamma, 
-                                            y = y_logGamma)) + # 曲線
-  geom_line(data = straight_line, mapping = aes(x = x_line, 
-                                                y = y_line), color = "#00A968") + # 直線
-  geom_line(data = tangent, mapping = aes(x = x_tangent, 
-                                          y = y_tangent), color = "Orange") + # 接線
-  geom_point(data = points, mapping = aes(x = x, y = y)) + 
-  labs(x = "x", y = "y") # ラベル
+  geom_line(data = log_Gamma_df, mapping = aes(x = x, y = y)) + # 曲線
+  geom_line(data = straight_line_df, mapping = aes(x = x, y = y), color = "#00A968") + # 直線
+  geom_line(data = tangent_df, mapping = aes(x = x, y = y), color = "Orange") + # 接線
+  geom_point(data = point_df, mapping = aes(x = x, y = y)) +  # 曲線上の2点
+  labs(title = "log Gamma Function", 
+       y = expression(paste(log, Gamma, "(x)", sep = ""))) # ラベル
 
