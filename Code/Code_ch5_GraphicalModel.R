2d610dfae32e29c7d0edd5f280630a84ae5d9bf8
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-10-05 22:27:24 +0900

    upload ch5.3 graphical model

--- Code/Code_ch5_GraphicalModel.R
+++ Code/Code_ch5_GraphicalModel.R
@@ -69,7 +69,7 @@ DiagrammeR::grViz("
 
     subgraph cluster_D{
       label = D
-      rank = same; z; y;
+      
       theta [label = <<B>&theta;</B>@_{d}>]
       
       subgraph cluster_N{
@@ -107,4 +107,68 @@ DiagrammeR::grViz("
       psi -> gamma[dir = back];
   }
 ")
+
+
+# ch5.3 ノイズあり対応トピックモデル -----------------------------------------------------------
+
+# ノイズあり対応トピックモデル
+DiagrammeR::grViz("
+  digraph NoisyCorrTM{
+    graph [rankdir = LR]
+    node [shape = circle, fixedsize = ture, fontname = 'Times-Italic']
+    
+    alpha [label = '&alpha;']
+    eta [label = '&eta;']
+    lambda [label = '&lambda;']
+    {rank = same; alpha; lambda}
+    
+    subgraph cluster_D{
+      label = D
+      
+      theta [label = <<B>&theta;</B>@_{d}>]
+      
+      subgraph cluster_N{
+        label = 'N@_{d}'
+
+        z [label = 'z@_{dn}']
+        w [label = 'w@_{dn}', style = filled, filledcolor = 'gray']
+      }
+      
+      subgraph cluster_M{
+        label = 'M@_{d}'
+        
+        y [label = 'y@_{dm}']
+        r [label = 'r@_{dm}']
+        x [label = 'x@_{dm}', style = filled, filledcolor = 'gray']
+      }
+    }
+    
+    beta [label = '&beta;']
+    gamma [label = '&gamma;']
+    {rank = same; beta; gamma}
+    
+    subgraph cluster_K{
+      label = K
+      
+      phi [label = <<B>&phi;</B>@_{k}>]
+    }
+    
+    subgraph cluster_K1{
+      label = 'K+1'
+      
+      psi [label = <<B>&psi;</B>@_{k}>]
+    }
+    
+    edge []
+      alpha -> theta -> z;
+      z -> {w, y};
+      w -> phi[dir = back];
+      phi -> beta[dir = back];
+      eta -> lambda -> r;
+      {y, r} -> x;
+      x -> psi[dir = back];
+      psi -> gamma[dir = back];
+  }
+")
+
 
