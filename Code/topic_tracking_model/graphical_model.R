3d01d1cb8b3b2337107b4f10ed606dea98e71eb4
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2025-05-19 16:16:22 +0900

    「長期間の依存関係を持つ場合」を追加

--- Code/topic_tracking_model/graphical_model.R
+++ Code/topic_tracking_model/graphical_model.R
@@ -11,6 +11,8 @@ library(DiagrammeRsvg)
 
 # グラフィカルモデルの作図 ------------------------------------------------------------
 
+### ・1時刻のパラメータの場合 -----
+
 # トピック追跡モデルのグラフィカルモデルを作図
 graph <- DiagrammeR::grViz("
   digraph dot{
@@ -115,5 +117,166 @@ DiagrammeRsvg::export_svg(gv = graph) |> # svgファイルに変換
   charToRaw() |> 
   rsvg::rsvg(height = 1500) |> # ビットマップに変換
   png::writePNG(target = "figure/graphical_model/topic_tracking_model.png", dpi = 100) # pngファイルに変換
+
+
+### ・複数時刻のパラメータの場合 -----
+
+# トピック追跡モデルのグラフィカルモデルを作図
+graph <- DiagrammeR::grViz("
+  digraph dot{
+    
+    graph [rankdir = LR, newrank = true]
+    node  [shape = circle, fixedsize = ture, height = 0.6, fontname = 'Times-Italic']
+    edge  []
+    
+    subgraph cluster_t_fin{
+      label = ' '
+      color = 'white'
+      
+      theta_fin [label = ' ', shape = none]
+      phi_fin   [label = ' ', shape = none]
+      
+      theta_fin -> phi_fin [penwidth = 0, dir = none];
+    }
+    
+    
+    subgraph cluster_t_dummy{
+      color = 'white'
+      
+      alpha_t [label = '&alpha;@_{d}@^{(t)}']
+      
+      subgraph cluster_d_t{
+        label = 'D'
+        color = 'black'
+        
+        theta_t [label = <<B>&theta;</B>@_{d}@^{(t)}>]
+        
+        subgraph cluster_n_t{
+          label = 'N@_{d}'
+          color = 'black'
+          
+          z_t [label = 'z@_{dn}@^{(t)}']
+          w_t [label = 'w@_{dn}@^{(t)}', style = filled, filledcolor = gray]
+        }
+      }
+      
+      beta_t [label = '&beta;@_{k}@^{(t)}']
+      
+      subgraph cluster_k_t{
+        label     = 'K'
+        labeljust = r
+        color = 'black'
+        
+        phi_t [label = <<B>&phi;</B>@_{'k}@^{(t)}>]
+      }
+    }
+    
+    subgraph cluster_t1_dummy{
+      color = 'white'
+      
+      alpha_t1 [label = '&alpha;@_{d}@^{(t-1)}']
+      
+      subgraph cluster_d_t1{
+        label = 'D'
+        color = 'black'
+        
+        theta_t1 [label = <<B>&theta;</B>@_{d}@^{(t-1)}>]
+        
+        subgraph cluster_n_t1{
+          label = 'N@_{d}'
+          color = 'black'
+          
+          z_t1 [label = 'z@_{dn}@^{(t-1)}']
+          w_t1 [label = 'w@_{dn}@^{(t-1)}', style = filled, filledcolor = gray]
+        }
+      }
+      
+      beta_t1 [label = '&beta;@_{k}@^{(t-1)}']
+      
+      subgraph cluster_k_t1{
+        label     = 'K'
+        labeljust = r
+        color = 'black'
+        
+        phi_t1 [label = <<B>&phi;</B>@_{'k}@^{(t-1)}>]
+      }
+    }
+    
+    
+    subgraph cluster_t2_dummy{
+      color = 'white'
+      
+      alpha_t2 [label = '&alpha;@_{d}@^{(t-2)}']
+      
+      subgraph cluster_d_t2{
+        label = 'D'
+        color = 'black'
+        
+        theta_t2 [label = <<B>&theta;</B>@_{d}@^{(t-2)}>]
+        
+        subgraph cluster_n_t2{
+          label = 'N@_{d}'
+          color = 'black'
+          
+          z_t2 [label = 'z@_{dn}@^{(t-2)}']
+          w_t2 [label = 'w@_{dn}@^{(t-2)}', style = filled, filledcolor = gray]
+        }
+      }
+      
+      beta_t2 [label = '&beta;@_{k}@^{(t-2)}']
+      
+      subgraph cluster_k_t2{
+        label     = 'K'
+        labeljust = r
+        color = 'black'
+        
+        phi_t2 [label = <<B>&phi;</B>@_{'k}@^{(t-2)}>]
+      }
+    }
+    
+    subgraph cluster_t_init{
+      color = 'white'
+      
+      label    = 'topic tracking model'
+      labelloc = c
+      fontsize = 20
+      
+      theta_init [label = ' ', shape = none]
+      phi_init   [label = ' ', shape = none]
+      
+      theta_init -> phi_init [penwidth = 0, dir = none];
+    }
+    
+    
+    {rank = same; theta_init; theta_t2; theta_t1; theta_t; theta_fin}
+    {rank = same; phi_init; phi_t2; phi_t1; phi_t; phi_fin}
+    
+    alpha_t2 -> theta_t2 -> z_t2 -> w_t2;
+    w_t2 -> phi_t2 -> beta_t2 [dir = back];
+    
+    alpha_t1 -> theta_t1 -> z_t1 -> w_t1;
+    w_t1 -> phi_t1 -> beta_t1 [dir = back];
+    
+    alpha_t -> theta_t -> z_t -> w_t;
+    w_t -> phi_t -> beta_t [dir = back];
+    
+    theta_init -> theta_t2 -> theta_t1;
+    {theta_t2, theta_t1} -> theta_t -> theta_fin;
+    phi_init -> phi_t2 -> phi_t1;
+    {phi_t2, phi_t1} -> phi_t -> phi_fin;
+  }
+")
+
+## ( `{'k}` は書き出し時にφの添字が重なってしまう対策用の小細工)
+## ( `_init, _fin` のクラスタは時間軸方向のエッジの長さ調整用の小細工)
+## (ダミークラスタの余白が空くので図タイトルをクラスタタイトルで代用)
+## ( 変数の定義順と上下の表示順が逆順になるのが謎)
+## ( dummy clusterは時刻ごとの縦方向のズレの調整用の小細工)
+
+# グラフを書出
+DiagrammeRsvg::export_svg(gv = graph) |> # svgファイルに変換
+  charToRaw() |> 
+  rsvg::rsvg(height = 2000) |> # ビットマップに変換
+  png::writePNG(target = "figure/graphical_model/topic_tracking_model_LongDeps.png", dpi = 100) # pngファイルに変換
 
 
